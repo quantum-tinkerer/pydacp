84ff186de3bf04929d92a279b640acf04f254575
antoniolrm <antoniolrm@usp.br>
2022-02-02 10:51:44 +0100

    slow and unstable version of updated block method

--- src/pyDACP/chebyshev.py
+++ src/pyDACP/chebyshev.py
@@ -1,6 +1,8 @@
 import numpy as np
-from scipy.linalg import qr, qr_insert
+from scipy.linalg import qr, qr_insert, eigvalsh
 import itertools as it
+import matplotlib.pyplot as plt
+
 
 def chebyshev_recursion_gen(matrix, v_0):
     """
@@ -44,7 +46,7 @@ def low_E_filter(v_rand, F_operator, K_max):
     chebyshev_recursion = chebyshev_recursion_gen(F_operator, v_rand)
     for i in range(K_max + 1):
         v_n = next(chebyshev_recursion)
-    return v_n/np.linalg.norm(v_n, axis=0)
+    return v_n / np.linalg.norm(v_n, axis=0)
 
 
 def basis(v_proj, G_operator, dk, first_run=True, Q=None, R=None):
@@ -102,7 +104,7 @@ def index_generator_fn(dk):
             yield i + 1
         else:
             for item in items:
-                result = dk*i+item
+                result = dk * i + item
                 if result == prev_result:
                     continue
                 prev_result = result
@@ -110,7 +112,18 @@ def index_generator_fn(dk):
         i += 1
 
 
-def basis_no_store(v_proj, G_operator, matrix, dk, random_vectors):
+def basis_no_store(
+    v_proj,
+    G_operator,
+    matrix,
+    dk,
+    random_vectors,
+    v_prev=None,
+    first_run=True,
+    S_prev=None,
+    matrix_prev=None,
+    indices_prev=None,
+):
     """
     Generate a complete basis with Chebyshev evolution.
 
@@ -130,9 +143,10 @@ def basis_no_store(v_proj, G_operator, matrix, dk, random_vectors):
     S_xy = []
     matrix_xy = []
     index_generator = index_generator_fn(dk)
+    # print(v_proj.shape)
     chebyshev_recursion = chebyshev_recursion_gen(G_operator, v_proj)
     storage_list = [next(index_generator)]
-    k_list = [0, dk-1, dk]
+    k_list = [0, dk - 1, dk]
     k_latest = 0
     index = 1
     while True:
@@ -141,7 +155,8 @@ def basis_no_store(v_proj, G_operator, matrix, dk, random_vectors):
             storage_list.append(next(index_generator))
             S_xy.append(v_proj.conj().T @ v_n)
             matrix_xy.append(v_proj.conj().T @ matrix @ v_n)
-        if 2*index*dk + 1 == k_latest: #not sure whether to +1 or not so delete maybe sometime future
+        if 2 * index * dk + 1 == k_latest:
+            # not sure whether to +1 or not so delete maybe sometime future
             k_products = np.array(list(it.product(k_list, k_list)))
             xpy = np.sum(k_products, axis=1).astype(int)
             xmy = np.abs(k_products[:, 0] - k_products[:, 1]).astype(int)
@@ -151,24 +166,144 @@ def basis_no_store(v_proj, G_operator, matrix, dk, random_vectors):
 
             s_xy = np.asarray(S_xy)
             m_xy = np.asarray(matrix_xy)
-            S = 0.5 * (s_xy[ind_p] + s_xy[ind_m])
-            matrix_proj = 0.5 * (m_xy[ind_p] + m_xy[ind_m])
+            S_diag = 0.5 * (s_xy[ind_p] + s_xy[ind_m])
+            matrix_proj_diag = 0.5 * (m_xy[ind_p] + m_xy[ind_m])
             m = len(k_list)
 
-            S = np.reshape(S, (m, m, random_vectors, random_vectors))
-            S = np.hstack(np.hstack(S))
-            norms = np.outer(np.diag(S), np.diag(S))
-            S = np.multiply(S, norms)
-            matrix_proj = np.reshape(matrix_proj, (m, m, random_vectors, random_vectors))
-            matrix_proj = np.hstack(np.hstack(matrix_proj))
-            matrix_proj = np.multiply(matrix_proj, norms)
-            q_S, r_S = qr(S)
-            ortho_condition = np.abs(np.diag(r_S)) < 1e-12
-            if ortho_condition.any():
-                indices = np.invert(ortho_condition)
-                return S[indices, :][:, indices], matrix_proj[indices, :][:, indices]
+            shape_diag = (
+                m,
+                m,
+                random_vectors,
+                random_vectors,
+            )
+
+            S_diag = np.reshape(S_diag, shape_diag)
+            S_diag = np.concatenate(
+                np.concatenate(
+                    S_diag,
+                    axis=1,
+                ),
+                axis=1,
+            )
+            norms_diag = 1 / np.sqrt(np.diag(S_diag))
+            norms = np.outer(norms_diag, norms_diag)
+            S_diag = np.multiply(S_diag, norms)
+            matrix_proj_diag = np.reshape(matrix_proj_diag, shape_diag)
+            matrix_proj_diag = np.concatenate(
+                np.concatenate(
+                    matrix_proj_diag,
+                    axis=1,
+                ),
+                axis=1,
+            )
+            matrix_proj_diag = np.multiply(matrix_proj_diag, norms)
+
+            if first_run:
+                S = S_diag
+                matrix_proj = matrix_proj_diag
+
             else:
+                # Construct block off-diagonal
+
+                # This part can be made much faster using the Chebyshev relation:
+                # T_i(x) * T_j(x) = 0.5 * (T_{i+j}(x) + T_{|i-j|}(x))
+                # And probably can reuse values already computed in the previous step.
+
+                # This part goes as follows:
+                # We want to compute Hij and Sij.
+                # i goes from 1 to n
+                # j goes from 1 to k_list[-1] / dk
+                # As you can see, n is different for different iterations.
+                # Obtain list of indices used to construct S_diag
+
+                Sij_prev = []
+                matrix_ij_prev = []
+                for i, n in enumerate(indices_prev):
+                    Sij_prev_n = []
+                    matrix_ij_prev_n = []
+                    k_prev_1 = np.arange(1, n + 1, 1) * dk
+                    k_prev_2 = k_prev_1 - 1
+                    k_prev_list = np.unique(np.concatenate([[0], k_prev_1, k_prev_2]))
+                    # Chebolve random vectors from previous run
+                    chebyshev_recursion_prev_off = chebyshev_recursion_gen(
+                        G_operator, v_prev[i]
+                    )
+                    for k_prev in range(k_prev_list[-1]+1):
+                        # Generate next vector in the Chebolution
+                        v_prev_off = next(chebyshev_recursion_prev_off)
+                        # Loop over j's
+                        Sj_prev_n = []
+                        matrix_j_prev_n = []
+                        # This is storing all i's
+                        if k_prev in k_prev_list:
+                            # Chebolve random vectors from this run
+                            chebyshev_recursion_proj_off = chebyshev_recursion_gen(
+                                G_operator, v_proj
+                            )
+                            for k_proj in range(k_list[-1]+1):
+                                # Generate next vector in the Chebolution
+                                v_proj_off = next(chebyshev_recursion_proj_off)
+                                # This is storing all j's
+                                if k_proj in k_list:
+                                    # Normalize
+                                    v_a = v_proj_off / np.linalg.norm(
+                                        v_proj_off, axis=0
+                                    )
+                                    v_b = v_prev_off / np.linalg.norm(
+                                        v_prev_off, axis=0
+                                    )
+                                    Sj_prev_n.append(v_b.conj().T @ v_a)
+                                    matrix_j_prev_n.append(v_b.conj().T @ matrix @ v_a)
+                            Sij_prev_n.append(np.stack(Sj_prev_n))
+                            matrix_ij_prev_n.append(np.stack(matrix_j_prev_n))
+                    Sij_prev.append(np.stack(Sij_prev_n))
+                    matrix_ij_prev.append(np.stack(matrix_ij_prev_n))
+
+                S_off = np.vstack(Sij_prev)
+                S_off = np.concatenate(
+                    np.concatenate(
+                        S_off,
+                        axis=1,
+                    ),
+                    axis=1,
+                )
+                matrix_proj_off = np.vstack(matrix_ij_prev)
+                matrix_proj_off = np.concatenate(
+                    np.concatenate(
+                        matrix_proj_off,
+                        axis=1,
+                    ),
+                    axis=1,
+                )
+
+                # Glue everything
+                S1 = np.hstack([S_prev, S_off])
+                S2 = np.hstack([S_off.conj().T, S_diag])
+                S = np.vstack([S1, S2])
+                M1 = np.hstack([matrix_prev, matrix_proj_off])
+                M2 = np.hstack([matrix_proj_off.conj().T, matrix_proj_diag])
+                matrix_proj = np.vstack([M1, M2])
+
+            # Perform QR orthogonalization of overlap matrix
+            # q_S, r_S = qr(S)
+            # ortho_condition = np.abs(np.diag(r_S)) < 1e-6
+            s = eigvalsh(S)
+            indx = np.abs(s) > 1e-3
+            dim = sum(indx)
+            # if ortho_condition.any():
+            if dim < S.shape[0]:
+                # qr_idx = np.invert(ortho_condition)
+                # dim = sum(qr_idx)
+                print("Found " + str(dim) + " vectors so far.")
+                if first_run:
+                    indices_chebolution = [index]
+                else:
+                    indices_chebolution = indices_prev.copy()
+                    indices_chebolution.append(index)
+                return S, matrix_proj, dim, indices_chebolution
+            else:
+                # S_prev, matrix_prev = S, matrix_proj
                 index += 1
-                k_list.append(k_list[-1]+dk-1)
-                k_list.append(k_list[-2]+dk)
+                k_list.append(k_list[-1] + dk - 1)
+                k_list.append(k_list[-2] + dk)
         k_latest += 1
--- src/pyDACP/core.py
+++ src/pyDACP/core.py
@@ -1,9 +1,10 @@
 from scipy.sparse.linalg import eigsh
 from scipy.sparse import eye, csr_matrix
-from scipy.linalg import eigh, eigvalsh
+from scipy.linalg import eigh, eigvalsh, qr, qr_insert
 from . import chebyshev
 import numpy as np
 from math import ceil
+import matplotlib.pyplot as plt
 
 
 def dacp_eig(
@@ -71,7 +72,7 @@ def dacp_eig(
     Ec = (Emax ** 2 + a ** 2) / 2
     F_operator = (matrix @ matrix - eye(matrix.shape[0]) * Ec) / E0
 
-    def get_filtered_vector():
+    def get_filtered_vector(qr_decomp = False):
         v_rand = 2 * (
             np.random.rand(matrix.shape[0], random_vectors)
             + np.random.rand(matrix.shape[0], random_vectors) * 1j
@@ -80,7 +81,10 @@ def dacp_eig(
         v_rand = v_rand / np.linalg.norm(v_rand, axis=0)
         K_max = int(filter_order * np.max(np.abs(bounds)) / a)
         vec = chebyshev.low_E_filter(v_rand, F_operator, K_max)
-        return vec / np.linalg.norm(vec, axis=0)
+        if random_vectors > 1 and qr_decomp:
+            return qr(vec / np.linalg.norm(vec, axis=0), mode='economic')
+        else:
+            return vec / np.linalg.norm(vec, axis=0)
 
     a_r = a / np.max(np.abs(bounds))
     dk = np.pi / a_r
@@ -118,179 +122,100 @@ def dacp_eig(
 
     else:
         # First run
-        S, matrix_proj = chebyshev.basis_no_store(
-            v_proj=get_filtered_vector(),
+        # Generate a QR orthogonalized collection of random vectors.
+        Q, R = get_filtered_vector(qr_decomp=True)
+        # Make sure they are indeed orthogonal by removing the non_orthogonal ones.
+        # MUST BE CAREFUL HERE BECAUSE THE COLLECTION OF VECTORS IS NO LONGER SIZE (M, random_vectors)
+        # So we update everywhere `random_vectors = filtered_qr_vectors.shape[1]`.
+        ortho_condition = np.abs(np.diag(R)) < 1e-9
+        if ortho_condition.any():
+            indices = np.invert(ortho_condition)
+            Q, R = Q[:, indices], R[indices, :][:, indices]
+        # Now run
+        S, matrix_proj, dim, indices_prev = chebyshev.basis_no_store(
+            v_proj=Q,
             G_operator=G_operator,
             matrix=matrix,
             dk=ceil(dk),
-            random_vectors=random_vectors
+            random_vectors=Q.shape[1],
         )
-        
         # Second run
-        Qi, Ri = chebyshev.basis(
-            v_proj=get_filtered_vector(),
+        # Same thing, but we don't perform QR in the first step.
+        v_i = get_filtered_vector()
+        # Instead, we QR wrt all the vectors collected so far.
+        Q_i, R_i = qr_insert(Q, R, u=v_i, k=Q.shape[1], which="col")
+        # Remove non-orthogonal vectors
+        ortho_condition = np.abs(np.diag(R_i)) < 1e-9
+        if ortho_condition.any():
+            indices = np.invert(ortho_condition)
+            Q_i, R_i = Q_i[:, indices], R_i[indices, :][:, indices]
+        # Q_i contain all vectors, we just want the new ones.
+        v_i = Q_i[:,Q.shape[1]:]
+
+        # This is just to adjust the dimenstion:
+        v_prev = np.stack([Q])
+        Si, matrix_proj_i, dim_i, indices_prev = chebyshev.basis_no_store(
+            v_proj=v_i,
+            v_prev=v_prev,
             G_operator=G_operator,
-            dk=dk,
-            Q=Q,
-            R=R,
+            matrix=matrix,
+            dk=ceil(dk),
+            random_vectors=v_i.shape[1],
+            S_prev = S,
+            matrix_prev = matrix_proj,
             first_run=False,
+            indices_prev = indices_prev
         )
+        # Again adjust dimenstion
+        v_prev_i = np.stack([v_i])
 
-        while
+        # We have two stop conditions here:
+        # 1. Dimension of the subspace no longer increases.
+        # 2. There are no more orthogonal random vectors.
+        while (dim_i > dim) and (v_i.shape[1] > 0):
+            # If both conditions are fulfilled, we go to next interation.
+            S, matrix_proj, R, Q, dim = Si, matrix_proj_i, R_i, Q_i, dim_i
+            # Stack vectors together and make sure dimensions match.
+            v_prev = np.vstack([v_prev, v_prev_i])
+            # Generate a new set of random vectors.
+            v_i = get_filtered_vector()
+            # Remove linearly dependent ones.
+            Q_i, R_i = qr_insert(Q, R, u=v_i, k=Q.shape[1], which="col")
+            ortho_condition = np.abs(np.diag(R_i)) < 1e-9
+            if ortho_condition.any():
+                indices = np.invert(ortho_condition)
+                Q_i, R_i = Q_i[:, indices], R_i[indices, :][:, indices]
+            v_i = Q_i[:,Q.shape[1]:]
+            # If there's any vector to input, we run it one more time.
+            if v_i.shape[1] > 0:
+                Si, matrix_proj_i, dim_i, indices_prev = chebyshev.basis_no_store(
+                    v_proj=v_i,
+                    v_prev=v_prev,
+                    G_operator=G_operator,
+                    matrix=matrix,
+                    dk=int(dk),
+                    random_vectors=v_i.shape[1],
+                    S_prev = S,
+                    matrix_prev = matrix_proj,
+                    first_run=False,
+                    indices_prev = indices_prev
+                )
+                # Adjust dimension of vector.
+                v_prev_i = np.stack([v_i])
 
-        # s, V = eigh(S)
-        # indx = np.abs(s) > 1e-12
-        # lambda_s = np.diag(1/np.sqrt(s[indx]))
-        # U = V[:, indx]@lambda_s
+        # Normalization
+        # norms = np.outer(np.diag(Si), np.diag(Si))
+        # Si = np.multiply(Si, norms)
+        # matrix_proj_i = np.multiply(matrix_proj_i, norms)
 
-        return eigvalsh(matrix_proj, S)#U.T.conj() @ matrix_proj @ U)
+        return matrix_proj_i, Si
+        # # return eigvalsh(matrix_proj_i, Si)
 
 
-# TODO: Delete the class in the future
-class DACP_reduction:
-    def __init__(
-        self,
-        matrix,
-        a,
-        eps=0.1,
-        bounds=None,
-        random_vectors=2,
-        return_eigenvectors=False
-    ):
-        """Find the spectral bounds of a given matrix.
+#         s, V = eigh(Si)
+#         indx = s > 1e-12
+#         lambda_s = np.diag(1/np.sqrt(s[indx]))
+#         U = V[:, indx]@lambda_s
 
-        Parameters
-        ----------
-        matrix : 2D array
-            Initial matrix.
-        eps : float
-            Ensures that the bounds are strict.
-        bounds : tuple, or None
-            Boundaries of the spectrum. If not provided the maximum and
-            minimum eigenvalues are calculated.
-        random_vectors : int
-            When return_eigenvectors=False, specifies the maximum expected
-            degeneracy of the matrix.
-        return_eigenvectors : bool
-            If True, returns eigenvectors. However, if False, the algorithm
-            conserves memory.
-        """
-        self.matrix = csr_matrix(matrix)
-        self.a = a
-        self.eps = eps
-        self.return_eigenvectors = return_eigenvectors
-        if bounds:
-            self.bounds = bounds
-        else:
-            self.find_bounds()
-        self.random_vectors = random_vectors
-
-    def find_bounds(self):
-        # Relative tolerance to which to calculate eigenvalues.  Because after
-        # rescaling we will add eps / 2 to the spectral bounds, we don't need
-        # to know the bounds more accurately than eps / 2.
-        tol = self.eps / 2
-
-        lmax = float(
-            eigsh(self.matrix, k=1, which="LA", return_eigenvectors=False, tol=tol)
-        )
-        lmin = float(
-            eigsh(self.matrix, k=1, which="SA", return_eigenvectors=False, tol=tol)
-        )
-
-        if lmax - lmin <= abs(lmax + lmin) * tol / 2:
-            raise ValueError(
-                "The matrix has a single eigenvalue, it is not possible to "
-                "obtain a spectral density."
-            )
-
-        self.bounds = [lmin, lmax]
-
-    def G_operator(self):
-        # TODO: generalize for intervals away from zero energy
-        Emin = self.bounds[0] * (1 + self.eps)
-        Emax = self.bounds[1] * (1 + self.eps)
-        E0 = (Emax - Emin) / 2
-        Ec = (Emax + Emin) / 2
-        return (self.matrix - eye(self.matrix.shape[0]) * Ec) / E0
-
-    def F_operator(self):
-        # TODO: generalize for intervals away from zero energy
-        Emax = np.max(np.abs(self.bounds)) * (1 + self.eps)
-        E0 = (Emax ** 2 - self.a ** 2) / 2
-        Ec = (Emax ** 2 + self.a ** 2) / 2
-        return (self.matrix @ self.matrix - eye(self.matrix.shape[0]) * Ec) / E0
-
-    def get_filtered_vector(self, filter_order=20):
-        v_rand = 2 * (
-            np.random.rand(self.matrix.shape[0], self.random_vectors)
-            + np.random.rand(self.matrix.shape[0], self.random_vectors) * 1j
-            - 0.5 * (1 + 1j)
-        )
-        v_rand = v_rand / np.linalg.norm(v_rand)
-        K_max = int(filter_order * np.max(np.abs(self.bounds)) / self.a)
-        vec = chebyshev.low_E_filter(v_rand, self.F_operator(), K_max)
-        return vec / np.linalg.norm(vec, axis=0)
-
-    def direct_eigenvalues(self):
-        a_r = self.a / np.max(np.abs(self.bounds))
-        dk = int(np.pi / a_r)
-
-        S, matrix_proj = chebyshev.basis_no_store(
-            v_proj=self.get_filtered_vector(),
-            matrix=self.G_operator(),
-            H=self.matrix,
-            dk=dk,
-            random_vectors=self.random_vectors
-        )
-
-        s, V = eigh(S)
-        indx = np.abs(s) > 1e-12
-        lambda_s = np.diag(1/np.sqrt(s[indx]))
-        U = V[:, indx]@lambda_s
-
-        return eigvalsh(U.T.conj() @ matrix_proj @ U)
-
-    def span_basis(self):
-        a_r = self.a / np.max(np.abs(self.bounds))
-        dk = np.pi / a_r
-        # First run
-        Q, R = chebyshev.basis(
-            v_proj=self.get_filtered_vector(),
-            matrix=self.G_operator(),
-            dk=dk
-        )
-        # Second run
-        Qi, Ri = chebyshev.basis(
-            v_proj=self.get_filtered_vector(),
-            matrix=self.G_operator(),
-            dk=dk,
-            Q=Q,
-            R=R,
-            first_run=False,
-        )
-        # Other runs to solve higher degeneracies
-        while Q.shape[1] < Qi.shape[1]:
-            Q, R = Qi, Ri
-            Qi, Ri = chebyshev.basis(
-                v_proj=self.get_filtered_vector(),
-                matrix=self.G_operator(),
-                dk=dk,
-                Q=Q,
-                R=R,
-                first_run=False,
-            )
-        self.v_basis = Q
-
-    def eigenvalues_and_eigenvectors(self):
-        self.span_basis()
-        S = self.v_basis.conj().T @ self.v_basis
-        matrix_proj = self.v_basis.conj().T @ self.matrix.dot(self.v_basis)
-        eigvals, eigvecs = eigh(matrix_proj)
-        return eigvals, eigvecs @ self.v_basis.T
-
-    def get_subspace_matrix(self):
-        if self.return_eigenvectors:
-            return self.eigenvalues_and_eigenvectors()
-        else:
-            return self.direct_eigenvalues()
+#         return eigvalsh(U.T.conj() @ matrix_proj_i @ U)
+        # return U.T.conj() @ matrix_proj_i @ U
