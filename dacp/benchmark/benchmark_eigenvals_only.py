6112cc6feb4f6433aa5fc228f5389e5b75e71125
antoniolrm <am@antoniomanesco.org>
2022-07-07 11:10:19 +0200

    updated benchmark script

--- dacp/benchmark/benchmark_eigenvals_only.py
+++ dacp/benchmark/benchmark_eigenvals_only.py
@@ -1,8 +1,7 @@
 import kwant
 import kwant.linalg.mumps as mumps
-from pyDACP import core, chebyshev
+import dacp
 import time
-from scipy.linalg import eigvalsh, eigh
 import scipy.sparse.linalg as sla
 from scipy.sparse import identity, diags
 import numpy as np
@@ -19,6 +18,7 @@ calculation = 'eigvals_only'
 t = 1
 
 def make_syst(N=100, dimension=2):
+    mu=2*dimension-1
 
     if dimension==2:
         L=np.sqrt(N)
@@ -46,7 +46,7 @@ def make_syst(N=100, dimension=2):
 
     def onsite(site, seed):
         delta_mu = kwant.digest.uniform(repr(site.pos) + str(seed)) - 0.5
-        return delta_mu - 2
+        return delta_mu - mu
 
     if dimension==2:
         syst[lat.shape(shape, (0, 0))] = onsite
@@ -87,14 +87,25 @@ def benchmark(N, seed, dimension):
     lmax = float(sla.eigsh(H, k=1, which="LA", return_eigenvectors=False))
     lmin = float(sla.eigsh(H, k=1, which="SA", return_eigenvectors=False))
     a = np.abs(lmax - lmin) / 2 * N ** (1 / 2) / N
+    print(
+        "Starting DACP step. Iteration "
+        + str(int(seed))
+        + ". "
+        + str(int(dimension))
+        + "-dimensional system. "
+        + str(int(N))
+        + " atoms."
+    )
+    print(a)
 
     def dacp_benchmark():
-        eigs = core.dacp_eig(
+        eigs = dacp.dacp.eigh(
             H,
             window_size=a,
             eps=0.05,
             return_eigenvectors=False,
             filter_order=15,
+            error_window=0.0,
             random_vectors=5,
         )
         return len(eigs)
@@ -104,6 +115,15 @@ def benchmark(N, seed, dimension):
     dacp_time = time.time() - start_time
 
     dacp_data = [dacp_time, dacp_memory]
+    print(
+        "Starting sparse diagonalization step. Iteration "
+        + str(int(seed))
+        + ". "
+        + str(int(dimension))
+        + "-dimensional system. "
+        + str(int(N))
+        + " atoms."
+    )
 
     def sparse_benchmark():
         _ = sparse_diag(H, sigma=0, k=k, which="LM", return_eigenvectors=False)
@@ -119,9 +139,9 @@ def benchmark(N, seed, dimension):
 
 # +
 params = {
-    'N' : [10**i for i in np.linspace(3, 5, 5, endpoint=True)],
+    'N' : [10**i for i in np.linspace(3, 4.5, 5, endpoint=True)],
     'seeds' : [1, 2, 3],
-    'dimensions' : [1, 2, 3]
+    'dimensions' : [2, 3]
 }
 
 values = list(params.values())
@@ -249,3 +269,22 @@ plt.xlabel(r'$\mathrm{Number\ of\ sites}$')
 plt.tight_layout()
 plt.savefig('mem_3d_' + calculation + '.png')
 plt.show()
+
+
+
+
+
+
+
+N=1000
+dimension=3
+seed=1
+syst = make_syst(N=N, dimension=dimension)
+H = syst.hamiltonian_submatrix(params=dict(seed=seed), sparse=True)
+lmax = float(sla.eigsh(H, k=1, which="LA", return_eigenvectors=False))
+lmin = float(sla.eigsh(H, k=1, which="SA", return_eigenvectors=False))
+print(lmin, lmax)
+
+
+
+
