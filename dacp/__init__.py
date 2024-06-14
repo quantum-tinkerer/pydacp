637bfda54bb9fae63e14de1d41a1f76b7fcde58c
Antonio Manesco <am@antoniomanesco.org>
2024-06-14 17:00:05 +0200

    include `estimate_errors`

--- dacp/__init__.py
+++ dacp/__init__.py
@@ -6,10 +6,11 @@ except ImportError:
     __version__ = "unknown"
     __version_tuple__ = (0, 0, "unknown", "unknown")
 
-from .solver import eigvalsh
+from .solver import eigvalsh, estimated_errors
 
 __all__ = [
     "eigvalsh",
+    "estimated_errors",
     "__version__",
     "__version_tuple__",
 ]
