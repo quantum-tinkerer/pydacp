8664cd8d8305fcedce7301afbd16fff98dccfde9
Antonio Manesco <am@antoniomanesco.org>
2024-06-14 17:01:06 +0200

    rename test file and improve variables' names

--- dacp/tests/test_eigvalsh.py
+++ dacp/tests/test_eigvalsh.py
@@ -1,5 +1,5 @@
 import numpy as np
-from dacp.solver import eigvalsh, estimated_errors
+from dacp import eigvalsh, estimated_errors
 from scipy.sparse import csr_matrix, kron
 from scipy.sparse import diags, eye
 import pytest
@@ -28,37 +28,39 @@ def random_ham_deg(N, deg):
     return csr_matrix(kron(H, eye(deg)))
 
 
-def eigv_errors(H, window, **dacp_kwargs):
-    eigv, _ = np.linalg.eigh(H.todense())
+def compute_errors(H, window, **dacp_kwargs):
+    eigvals_dense, _ = np.linalg.eigh(H.todense())
     if dacp_kwargs:
-        eigv_dacp = eigvalsh(H, window, **dacp_kwargs)
+        eigvals_dacp = eigvalsh(H, window, **dacp_kwargs)
     else:
-        eigv_dacp = eigvalsh(H, window)
+        eigvals_dacp = eigvalsh(H, window)
 
-    map_eigv = []
-    for value in eigv_dacp:
-        closest = np.abs(eigv - value).min()
-        map_eigv.append(eigv[np.abs(eigv - value) == closest][0])
-    map_eigv = np.array(map_eigv)
+    map_eigvals = []
+    for value in eigvals_dacp:
+        closest = np.abs(eigvals_dense - value).min()
+        map_eigvals.append(eigvals_dense[np.abs(eigvals_dense - value) == closest][0])
+    map_eigvals = np.array(map_eigvals)
 
-    map_eigv = map_eigv[np.argsort(np.abs(map_eigv))]
-    eigv_dacp = eigv_dacp[np.argsort(np.abs(eigv_dacp))]
+    map_eigvals = map_eigvals[np.argsort(np.abs(map_eigvals))]
+    eigvals_dacp = eigvals_dacp[np.argsort(np.abs(eigvals_dacp))]
 
-    relative_error = np.abs((eigv_dacp - map_eigv) / map_eigv)[map_eigv < window_size]
-    eta = estimated_errors(eigvals=eigv_dacp, tol=tol, filter_order=k, window=window)
+    relative_error = np.abs((eigvals_dacp - map_eigvals) / map_eigvals)[
+        map_eigvals < window_size
+    ]
+    eta = estimated_errors(eigvals=eigvals_dacp, tol=tol, filter_order=k, window=window)
 
     diff = relative_error - eta
     return np.log10(np.heaviside(diff, 0) * diff / eta)
 
 
-def eigv_errors_test(deg=False, **dacp_kwargs):
+def eigvals_errors_test(deg=False, **dacp_kwargs):
     relative_error_list = []
     if deg:
         H = random_ham_deg(N_block, deg_n)
     else:
         H = random_ham(N)
 
-    relative_error = eigv_errors(H, window, **dacp_kwargs)
+    relative_error = compute_errors(H, window, **dacp_kwargs)
     if relative_error.size != 0:
         relative_error_list.append(np.max(relative_error))
 
@@ -70,7 +72,7 @@ def test_eigvals():
     """
     Test the eigenvalue with non-degenerate hamiltonians
     """
-    error_diff = eigv_errors_test()
+    error_diff = eigvals_errors_test()
     assert error_diff.any(), "Errors don't match the theoretical value."
 
 
@@ -79,5 +81,5 @@ def test_eigvals_deg():
     """
     Test the eigenvalue method with degenerate hamiltonians
     """
-    error_diff = eigv_errors_test(deg=True, random_vectors=2)
+    error_diff = eigvals_errors_test(deg=True, random_vectors=2)
     assert error_diff.any(), "Errors don't match the theoretical value."
