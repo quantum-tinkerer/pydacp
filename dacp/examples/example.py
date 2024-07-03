9d171631af5391cda02487895dceeeb228ab46d5
Antonio Manesco <am@antoniomanesco.org>
2024-07-03 11:17:30 +0200

    jupytext-compatible example

--- dacp/examples/example.py
+++ dacp/examples/example.py
@@ -1,3 +1,17 @@
+# ---
+# jupytext:
+#   text_representation:
+#     extension: .md
+#     format_name: myst
+#     format_version: 0.13
+#     jupytext_version: 1.16.0
+# kernelspec:
+#   display_name: Python 3 (ipykernel)
+#   language: python
+#   name: python3
+# ---
+
+
 # -*- coding: utf-8 -*-
 # %%
 from dacp import estimated_errors, eigvalsh
